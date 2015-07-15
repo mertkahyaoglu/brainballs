2d58b2f8c58cc653b9456fd84b884c6bddf5966f
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-07-15 16:26:47 +0300

    visual update

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Brain Balls [(Demo)][1]
 ==========
 
+>Brain Balls is a memory game created with Phaser.js. The aim of the game is to remember sequences of balls.
 
+![](screenshot0.png)
 
-Brain Balls is a FirefoxOs Game created with Phaser.js. The aim of the game is to remember sequences of balls.
+Ready Screen               |  Win Screen
+:-------------------------:|:-------------------------:
+![](screenshot.png)        |  ![](screenshot1.png)
 
 [1]: http://mertkahyaoglu.github.io/brainballs/
--- js/screens/menu.js
+++ js/screens/menu.js
@@ -4,15 +4,10 @@ GameStates.Menu = {
 		this.add.sprite(0, 0, 'bg');
 		var logo = this.add.sprite(this.world.centerX, 120, 'logo');
 		logo.anchor.setTo(0.5, 0.5);
-		var btn_play = this.add.sprite(this.world.centerX, this.world.centerY + 30, 'play');
+		var btn_play = this.add.sprite(this.world.centerX, this.world.centerY + 50, 'play');
 		btn_play.anchor.setTo(0.5, 0.5);
 		btn_play.inputEnabled = true;
 		btn_play.events.onInputUp.addOnce(this.startGame, this);
-
-		var btn_scores = this.add.sprite(this.world.centerX, this.world.centerY + 100, 'scores');
-		btn_scores.anchor.setTo(0.5, 0.5);
-		btn_scores.inputEnabled = true;
-		btn_scores.events.onInputUp.addOnce(this.setScreen.bind(this, 'Scores'), this);
 	},
 
 	setScreen: function(screen) {
